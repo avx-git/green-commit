d93897e1e452cfc2b42bedbba76e1346d46d59bd
Aryan Vishwakarma <its.aryanvishwakarma.07@gmail.com>
2025-09-26 16:06:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,139 @@
+# 🌱 goGreen 
+
+With **goGreen**, you can make your profile look like you've been hard at work... even if you haven't. 
+NodeJs script to make commits to the past (or the future) to go green on GitHub.
+
+## About
+
+**goGreen** helps you create commits on your GitHub profile for any date in the past. Whether you want to fill up your contribution graph or even make cool patterns and artwork.
+
+## Getting Started
+
+Follow these steps to bring your contribution graph to life:
+
+1. **Clone this repository**
+```bash
+git clone https://github.com/fenrir2608/goGreen.git
+cd goGreen
+```
+3. **Set up your project**
+Initialize a new Node.js project:
+```bash
+npm init -y
+  ```
+3. **Install the required npm modules**
+You'll need a few modules to get everything running smoothly. Install them all with:
+  ```bash
+  npm install moment simple-git random
+  ```
+4. **Create your commit script**
+- Create a JavaScript file to manage your commits.
+- Create a JSON file to store all the commit timestamp data.
+
+## Room for Improvement
+
+So, you've got the basics down. What's next?
+
+- **Custom Patterns:** Experiment with different patterns on your contribution graph. Maybe spell out your name or create some cool designs.
+- **Density Control:** Play around with the number of commits per day to adjust the shades of green.
+- **Input Strings:** Convert input strings to X-Y mapped contributions.
+
+## npm Modules Used
+
+- [`moment`](https://www.npmjs.com/package/moment) - Handles date and time manipulation.
+- [`simple-git`](https://www.npmjs.com/package/simple-git) - For easy Git commands.
+- [`random`](https://www.npmjs.com/package/random) - To generate random numbers for the commits.
+
+## Credits
+
+Huge thanks to [Akshay Saini](https://github.com/akshaymarch7) for the original video behind this project.
+
+- Update 1260611844
+
+- Update 1757751022
+
+- Update 1661398388
+
+- Update 1280349189
+
+- Update 1105994071
+
+- Update 1352744608
+
+- Update 1181452759
+
+- Update 1689942868
+
+- Update 878043000
+
+- Update 729005152
+
+- Update 622065922
+
+- Update 388547085
+
+- Update 1728622421
+
+- Update 832115652
+
+- Update 1137504191
+
+- Update 1574410489
+
+- Update 102946154
+
+- Update 469702692
+
+- Update 1754476906
+
+- Update 1941736294
+
+- Update 478142480
+
+- Update 1998097915
+
+- Update 425895347
+
+- Update 1537924900
+
+- Update 1964671381
+
+- Update 1231771610
+
+- Update 792725080
+
+- Update 634606042
+
+- Update 1670682055
+
+- Update 1254867696
+
+- Update 1688261533
+
+- Update 717497213
+
+- Update 1045331463
+
+- Update 1195351761
+
+- Update 978134978
+
+- Update 1740547938
+
+- Update 681549933
+
+- Update 1819001437
+
+- Update 1983945266
+
+- Update 822388863
+
+- Update 1520966547
+
+- Update 1902964455
+
+- Update 768391895
+
+- Update 303207073
+
+- Update 10449569
---
+++ index.js
@@ -0,0 +1,178 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
+
+const path = "./data.json";
+
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
+};
+
+// Pattern for "AVX" on contribution graph
+const avxPattern = [
+  // Letter A (x: 0-4)
+  [0,1],[0,2],[0,3],[0,4],[0,5],
+  [1,0],[1,5],
+  [2,0],[2,1],[2,2],[2,3],[2,4],[2,5],
+  [3,0],[3,5],
+  [4,0],[4,5],
+  
+  // Letter V (x: 7-11)
+  [7,0],[7,1],[7,2],[7,3],[7,4],
+  [8,1],[8,2],[8,3],[8,4],
+  [9,2],[9,3],[9,4],
+  [10,3],[10,4],
+  [11,0],[11,1],[11,2],[11,3],[11,4],
+  
+  // Letter X (x: 14-18)
+  [14,0],[14,4],[14,5],
+  [15,1],[15,4],
+  [16,2],[16,3],
+  [17,1],[17,4],
+  [18,0],[18,4],[18,5],
+];
+
+let commitIndex = 0;
+
+const makePatternCommits = () => {
+  if(commitIndex >= avxPattern.length) {
+    console.log("Pattern complete! Pushing to GitHub...");
+    return simpleGit().push();
+  }
+  
+  const [x, y] = avxPattern[commitIndex];
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = { date: date };
+  console.log(`Creating commit at (${x}, ${y}): ${date}`);
+  
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }, () => {
+      commitIndex++;
+      makePatternCommits();
+    });
+  });
+};
+
+makePatternCommits();
+
+// Change 1186696925
+
+// Change 1920523915
+
+// Change 1941035689
+
+// Change 523724807
+
+// Change 1246489405
+
+// Change 1677935370
+
+// Change 1856218208
+
+// Change 582076197
+
+// Change 1013845766
+
+// Change 1025200945
+
+// Change 948832446
+
+// Change 355011927
+
+// Change 752299526
+
+// Change 1398801474
+
+// Change 288633177
+
+// Change 2023309109
+
+// Change 424404510
+
+// Change 1612542475
+
+// Change 1914347548
+
+// Change 1871415864
+
+// Change 136251307
+
+// Change 1508383731
+
+// Change 1673907984
+
+// Change 690144081
+
+// Change 98909379
+
+// Change 434460733
+
+// Change 315091206
+
+// Change 2106712479
+
+// Change 1405656193
+
+// Change 873815396
+
+// Change 767356135
+
+// Change 374336374
+
+// Change 1687569075
+
+// Change 1297228172
+
+// Change 1378758911
+
+// Change 2062204793
+
+// Change 779041967
+
+// Change 1473364159
+
+// Change 925000415
+
+// Change 17085033
+
+// Change 341856747
+
+// Change 1898285628
+
+// Change 406093880
+
+// Change 1694611198
+
+// Change 1417466475
+
+// Change 1791660868
+
+// Change 1853600726
+
+// Change 1598316537
+
+// Change 1616634233
+
+// Change 1938747778
+
+// Change 877960834
